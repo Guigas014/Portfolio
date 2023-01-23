afef11c98240f11e819fe431757c758839c506ca
Guilherme Alves Araujo <guilherme-allves@hotmail.com>
2023-01-22 23:22:09 -0300

    cardapio-web - first commit

---
+++ cardapio-web/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      I'm hungry!!
+    </div>
+  )
+}
+
+export default App
